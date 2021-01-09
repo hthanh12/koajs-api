6257d3b1da308873da9b86f58e169a8f9d0f6ffe
hthanh12 <nghoangthanh1212@gmail.com>
2021-01-09 12:44:49 +0700

    fix get answer/sameResult

--- src/controller/answerController.js
+++ src/controller/answerController.js
@@ -1,17 +1,21 @@
 const { Answer } = require("../model");
 const response = require("../utils/response");
 const answerService = require("../service/answer");
-const { QueryTypes,sequelize } = require('sequelize');
-
+const { QueryTypes,sequelize, literal,Op } = require('sequelize');
 
 exports.sameResult = async (ctx) => {
   try {
     let {questionId, answerList} = ctx.params;
-    
-    const answer = await sequelize.query('SELECT * FROM "users"',{ type: QueryTypes.SELECT });
+    console.log(answerList)
+    const answer = await Answer.findAll({where: {
+      list_answers:{
+        [Op.contains]:[{"answer": 1, "id_question": 1}]
+      }
+    }})
     console.log(answer)
     return response.success(ctx, { data: { questionId, answerList } });
   } catch (error) {
+    console.log(error)
     return response.error(ctx, { error });
   }
 };
